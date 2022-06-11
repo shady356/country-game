9aadda093c24927c13ec40c2f51c5cb5b4108438
shady356 <heenriko@gmail.com>
2022-06-12 01:06:29 +0200

    feat: organize map

---
+++ src/components/Game.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+
+    <h1>Game</h1>
+    <EuropeMap />
+  </div>
+</template>
+
+<script>
+import EuropeMap from '@/components/EuropeMap'
+export default {
+  components: {
+    EuropeMap
+  }
+}
+</script>
+
+<style>
+</style>
