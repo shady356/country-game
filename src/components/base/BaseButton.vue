619f050bda93c7cdf85eaff44972d79d982db183
shady356 <heenriko@gmail.com>
2022-09-24 21:44:34 +0200

    feat: add button clicked feedback

--- src/components/base/BaseButton.vue
+++ src/components/base/BaseButton.vue
@@ -1,5 +1,8 @@
 <template>
-  <button :class="['base-button', sizeStyle, shapeStyle, themeStyle]" @click="click"><slot /></button>
+  <button :class="['base-button', sizeStyle, shapeStyle, themeStyle]"
+    @click="click">
+    <slot />
+  </button>
 </template>
 
 <script>
@@ -49,32 +52,48 @@ export default {
   align-items: center;
   justify-content: center;
   text-transform: uppercase;
+  transform: perspective(1px);
+  transition: transform 100ms ease-out;
+  -webkit-font-smoothing: subpixel-antialiased;
+  backface-visibility: hidden;
+  cursor: pointer;
 
   &--size-small {
     padding: var(--size-1) var(--size-3);
   }
+
   &--size-medium {
     padding: var(--size-3) var(--size-6);
     font-size: var(--size-5);
   }
+
   &--size-large {
     padding: var(--size-4) var(--size-8);
     font-size: var(--size-6);
   }
+
   &--shape-round {
     border-radius: var(--size-9);
   }
+
   &--shape-square {
     border-radius: var(--size-4);
   }
+
   &--theme-cta {
     background-color: #0be5c8;
     color: #084b42;
   }
+
   &--theme-standard {
     background-color: #fff;
     border-color: #D4D9EF;
     color: #616B9C;
+  }
+
+  &:active {
+    transform: scale(1.05) translateZ(0);
+    transition: transform 100ms ease-in;
   }
 }
 </style>
